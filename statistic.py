1de86c1b3229b9949803780e614e0135c82ba8e7
zearkiatos <caprilespe@outlook.com>
2022-10-12 00:00:50 -0300

    Resolve exercise

--- statistic.py
+++ statistic.py
@@ -1,6 +1,44 @@
-import utils
+import pandas
+import numpy
 
 
-data = utils.loading_data("./data/data.csv")
+data = pandas.read_csv("./data/data.csv")
 
-print(data)
+
+dataFrame = pandas.DataFrame(data)
+
+print(dataFrame)
+
+
+def calculate_statistics(downloads: pandas.DataFrame) -> pandas.DataFrame:
+
+    models = list(downloads["MODELO"].unique())
+    row = {}
+    new_list = []
+    index_list = []
+
+    for model in models:
+        download = downloads[(downloads["MODELO"] == model)
+                             & (downloads["PAGO"] > 0)]
+        pago = download["PAGO"]
+        starts = download["ESTRELLAS"]
+        comments = download[download["COMENTARIO"] == True]
+        if (len(download) > 0):
+            if (not model in index_list):
+                index_list.append(model)
+            row = {
+                "CANTIDAD": len(pago),
+                "PROMEDIO": round(pago.mean(), 2),
+                "MAXIMO": round(pago.max(), 2),
+                "MINIMO":  round(pago.min(), 2),
+                "ESTRELLAS": round(starts.mean(), 2),
+                "DESV. ESTRELLAS": 0 if numpy.isnan(starts.std(axis=0, skipna=False)) else round(starts.std(axis=0, skipna=False), 2),
+                "COMENTARIOs": len(comments)
+            }
+            new_list.append(row)
+
+    new_list_dataframe = pandas.DataFrame(new_list, index=index_list)
+    return new_list_dataframe
+
+
+print(calculate_statistics(dataFrame))
--- utils.py
+++ utils.py
@@ -1,16 +1,16 @@
 def loading_data(path: str) -> list:
     data_list = []
     file = open(path)
-    file.readline().split(",")
+    head = file.readline().split(",")
     line = file.readline()
     while len(line) > 0:
         data = line.split(",")
         data_dictionary = {}
-        data_dictionary["modelo"] = data[0]
-        data_dictionary["usuario"] = data[1]
-        data_dictionary["pago"] = float(data[2])
-        data_dictionary["estrellas"] = float(data[3])
-        data_dictionary["estrellas"] = bool(str.lower(data[4]))
+        data_dictionary[head[0]] = data[0]
+        data_dictionary[head[1]] = data[1]
+        data_dictionary[head[2]] = float(data[2])
+        data_dictionary[head[3]] = float(data[3])
+        data_dictionary[str.replace(head[4],"\n","")] = bool(str.lower(data[4]))
         data_list.append(data_dictionary)
 
         line = file.readline()
