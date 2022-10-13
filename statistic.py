e36851641eec8e02e3d9863942263822d3d113bf
zearkiatos <caprilespe@outlook.com>
2022-10-12 23:51:56 -0300

    Fix model type

--- statistic.py
+++ statistic.py
@@ -7,7 +7,12 @@ data = pandas.read_csv("./data/data.csv")
 
 dataFrame = pandas.DataFrame(data)
 
-print(dataFrame)
+def add_quantity(data:any, quantity:any)->int:
+
+    data["CANTIDAD_GRATUITOS"] = quantity
+
+    return data
+
 
 
 def calculate_statistics(downloads: pandas.DataFrame) -> pandas.DataFrame:
@@ -17,13 +22,25 @@ def calculate_statistics(downloads: pandas.DataFrame) -> pandas.DataFrame:
     new_list = []
     index_list = []
 
+    downloads["CANTIDAD_GRATUITOS"] = 0
+
+    for model in models:
+        pagos = downloads[(downloads["MODELO"] == model) & (downloads["PAGO"] == 0)]
+        index = list(pagos.index)
+        quantity = len(pagos)
+        if (len(index) > 0):
+            for i in index:
+                downloads.loc[i, ["CANTIDAD_GRATUITOS"]] = quantity  
+
     for model in models:
         download = downloads[(downloads["MODELO"] == model)
                              & (downloads["PAGO"] > 0)]
         pago = download["PAGO"]
         starts = download["ESTRELLAS"]
         comments = download[download["COMENTARIO"] == True]
-        if (len(download) > 0):
+        free_quantity = downloads[(downloads["MODELO"] == model) & (downloads["CANTIDAD_GRATUITOS"] == len(pago))]
+        are_all_free = len(free_quantity) > 0
+        if (len(download) > 0 and not are_all_free):
             if (not model in index_list):
                 index_list.append(model)
             row = {
@@ -33,12 +50,12 @@ def calculate_statistics(downloads: pandas.DataFrame) -> pandas.DataFrame:
                 "MINIMO":  round(pago.min(), 2),
                 "ESTRELLAS": round(starts.mean(), 2),
                 "DESV. ESTRELLAS": 0 if numpy.isnan(starts.std(axis=0, skipna=False)) else round(starts.std(axis=0, skipna=False), 2),
-                "COMENTARIOs": len(comments)
+                "COMENTARIOS": len(comments)
             }
             new_list.append(row)
 
     new_list_dataframe = pandas.DataFrame(new_list, index=index_list)
-    return new_list_dataframe
+    return new_list_dataframe.sort_index()
 
 
 print(calculate_statistics(dataFrame))
